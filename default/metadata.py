9b074f251765a094a2a4598fa8b907e5c8ec2003
martastain <martas@imm.cz>
2014-07-18 21:47:23 +0200

    Metadata update

--- default/metadata.py
+++ default/metadata.py
@@ -3,6 +3,20 @@
 
 from nx.constants import *
 
+
+ENUM_MTYPES = {
+    0 : "File",
+    1 : "Virtual"
+}
+
+ENUM_CTYPES = {
+    TEXT: "Text",
+    VIDEO: "Video",
+    AUDIO: "Audio",
+    IMAGE: "Image"
+}
+
+
 BASE_META_SET = [
 
 # NAMESPACE  TAG         EDITABLE SEARCHABLE CLASS   DEFAULT   SETTINGS
@@ -11,11 +25,11 @@ BASE_META_SET = [
 ("o",  "ctime",                  0, 0, DATETIME,   0,         False),
 ("o",  "mtime",                  0, 0, DATETIME,   0,         False),
 
-("a",  "media_type",             0, 0, INTEGER,    0,         False),    # FILE / VIRTUAL
-("a",  "content_type",           0, 0, INTEGER,    0,         False),    # VIDEO / AUDIO /  IMAGE / TEXT 
-("a",  "id_folder",              1, 0, INTEGER,    0,         False), 
-("a",  "origin",                 0, 0, TEXT,       "Unknown", False),    # "Import", "Acquisition", "Library", "Ingest", "Edit", "Playout 1" ....
-("a",  "status",                 0, 0, INTEGER,    0,         False),    # OFFLINE, ONLINE, CREATING, TRASHED, ARCHIVED
+("a",  "media_type",             0, 0, ENUM,       0,         ENUM_MTYPES),        # FILE / VIRTUAL
+("a",  "content_type",           0, 0, ENUM,       0,         ENUM_CTYPES),        # VIDEO / AUDIO /  IMAGE / TEXT 
+("a",  "id_folder",              1, 0, INTEGER,    0,         False),     
+("a",  "origin",                 0, 0, TEXT,       "Unknown", False),              # "Import", "Acquisition", "Library", "Ingest", "Edit", "Playout 1" ....
+("a",  "status",                 0, 0, INTEGER,    0,         False),              # OFFLINE, ONLINE, CREATING, TRASHED, ARCHIVED
 ("a",  "version_of",             0, 0, INTEGER,    0,         False),
 
 ("e",  "start",                  1, 0, DATETIME,   0,         False),
@@ -27,19 +41,18 @@ BASE_META_SET = [
 
 ("i",  "id_asset",               0, 0, INTEGER,    0,         False),
 ("i",  "id_bin",                 0, 0, INTEGER,    0,         False),
-("i",  "position",               0, 0, INTEGER,    0,         False),    # Order of the item within the bin
+("i",  "position",               0, 0, INTEGER,    0,         False),              # Order of the item within the bin
 
 
 #
 # Virtual tags 
 #
 
-("v", "rundown_symbol",          0, 0, -1,         0,         False),    # Primary symbol in rundown view (folder color for items, star for event promo)
-("v", "rundown_status",          0, 0, -1,         0,         False),    # OFFLINE, READY etc
-("v", "rundown_broadcast",       0, 0, -1,         0,         False),    # Scheduled start time of block/item
-("v", "rundown_scheduled",       0, 0, -1,         0,         False),    # Real computed start time of the item
-
-("E", "run_mode",                0, 0, INTEGER,    0,         False),    # Real computed start time of the item
+("v", "rundown_symbol",          0, 0, -1,         0,         False),              # Primary symbol in rundown view (folder color for items, star for event promo)
+("v", "rundown_status",          0, 0, -1,         0,         False),              # OFFLINE, READY etc
+("v", "rundown_broadcast",       0, 0, -1,         0,         False),              # Scheduled start time of block/item
+("v", "rundown_scheduled",       0, 0, -1,         0,         False),              # Real computed start time of the item
+("E", "run_mode",                0, 0, INTEGER,    0,         False),              # AUTO / MANUAL / SOFT AUTO / HARD AUTO
 
 #
 # Base metadata
@@ -49,18 +62,18 @@ BASE_META_SET = [
 ("A",  "path",                   1, 1, TEXT,        "",       False),
 ("A",  "article",                1, 1, BLOB,        "",       {"syntax" : "md"}),
 ("A",  "subclips",               0, 0, REGIONS,     "[]",     False),
-("A",  "meta_probed",            0, 0, BOOLEAN,     0,        False),    # If true, meta_probes would not overwrite non-technical metadata during update
+("A",  "meta_probed",            0, 0, BOOLEAN,     0,        False),              # If true, meta_probes would not overwrite non-technical metadata during update
 
 ("AI", "mark_in",                1, 0, TIMECODE,    0,        False),
 ("AI", "mark_out",               1, 0, TIMECODE,    0,        False),
 
-("AIEB",  "title",               1, 1, TEXT,        "",       False),    # dc.title.main - The title most commonly associated with the resource. Where no TYPE is specified, main is assumed. A main title is required for every resource.
-("AIEB",  "title/subtitle",      1, 1, TEXT,        "",       False),    # dc.title.subtitle - Ancillary title information for the resource. A main title is required before subtitle(s) may be used.
-("AIEB",  "title/alternate",     1, 1, TEXT,        "",       False),    # dc.title.alternate - Where a resource is known by more than one name, or where it has a formal name and a vernacular name, the alternate or vernacular name may be recorded here. A main title recording the full and formal name is required before an alternate title may be given.
-("AIEB",  "title/series",        1, 1, CS_SELECT,   "",       "series"), # dc.title.series - Where the resource is part of a series (CBA Research Reports, Star Trek, or whatever), the series name may be given here."
-("AIEB",  "title/original",      1, 1, TEXT,        "",       False),    # ebucore.title.original
+("AIEB",  "title",               1, 1, TEXT,        "",       False),              # dc.title.main - The title most commonly associated with the resource. Where no TYPE is specified, main is assumed. A main title is required for every resource.
+("AIEB",  "title/subtitle",      1, 1, TEXT,        "",       False),              # dc.title.subtitle - Ancillary title information for the resource. A main title is required before subtitle(s) may be used.
+("AIEB",  "title/alternate",     1, 1, TEXT,        "",       False),              # dc.title.alternate - Where a resource is known by more than one name, or where it has a formal name and a vernacular name, the alternate or vernacular name may be recorded here. A main title recording the full and formal name is required before an alternate title may be given.
+("AIEB",  "title/series",        1, 1, CS_SELECT,   "",       "series"),           # dc.title.series - Where the resource is part of a series (CBA Research Reports, Star Trek, or whatever), the series name may be given here."
+("AIEB",  "title/original",      1, 1, TEXT,        "",       False),              # ebucore.title.original
 ("AIEB",  "description",         1, 1, BLOB,        "",       {"syntax" : "md"}),
-("AIEB",  "promoted",            1, 0, BOOLEAN,     0,        False),    # Asset "promotion". It"s hit, important, favourite,....
+("AIEB",  "promoted",            1, 0, BOOLEAN,     0,        False),              # Asset "promotion". It"s hit, important, favourite,....
 
 
 #
@@ -69,13 +82,12 @@ BASE_META_SET = [
 
 ("m",  "language",               1, 0, CS_SELECT,   "en-US",  "languages"),
 ("m",  "date",                   1, 0, DATETIME,    0,        False),
-("m",  "coverage",               1, 1, BLOB,        "",       {"syntax":"off"}),
-("m",  "subject",                1, 1, BLOB,        "",       {"syntax":"off"}),   # Keywords
-("m",  "rights",                 1, 1, BLOB,        "",       {"syntax":"off"}),
+("m",  "subject",                1, 1, BLOB,        "",       False),              # Keywords
+("m",  "rights",                 1, 1, BLOB,        "",       False),
 ("m",  "version",                1, 1, TEXT,        "",       False),
 
 ("m",  "source",                 0, 1, TEXT,        "",       False),              # Youtube, Vimeo, PirateBay....
-("m",  "source/url",             0, 1, TEXT,        "",       False),              
+("m",  "source/url",             0, 1, TEXT,        "",       False),              # youtube url, torrent magnet link....
 
 ("m",  "format",                 1, 1, CS_SELECT,   "",       "formats"),          # documentary / featrure / clip / sport event / ....
 ("m",  "genre",                  1, 1, CS_SELECT,   "",       "genres"),           # horror / football / punk rock
@@ -83,22 +95,21 @@ BASE_META_SET = [
 ("m",  "identifier/main",        1, 1, TEXT,        "",       False),              # Primary Content ID (IDEC, GUID...)
 ("m",  "identifier/youtube",     0, 1, TEXT,        "",       False),              # Youtube ID if exists
 ("m",  "identifier/vimeo",       0, 1, TEXT,        "",       False),              # Vimeo ID if exists
-("m",  "identifier/imdb",        1, 1, TEXT,        "",       False),
+("m",  "identifier/imdb",        1, 1, TEXT,        "",       False),              # IMDB ID for movies
 
 ("m",  "role/director",          1, 1, TEXT,        "",       False),              # ebu_RoleCode 20.16
-("m",  "role/composer",          1, 1, TEXT,        "",       False),              # ebu_RoleCode 17.1.7 (music)
-("m",  "role/performer",         1, 1, TEXT,        "",       False),              # ebu_RoleCode 17.2   (music)
+("m",  "role/composer",          1, 1, TEXT,        "",       False),              # ebu_RoleCode 17.1.7 (music) 
+("m",  "role/performer",         1, 1, TEXT,        "",       False),              # ebu_RoleCode 17.2   (music) (A.K.A Artist)
 
-#
-# Following keys are not supported by EBUCore
-#
 
 ("m",  "album",                  1, 1, TEXT,        "",       False),
 ("m",  "album/track",            1, 0, INTEGER,     0,        False),
 ("m",  "album/disc",             1, 0, INTEGER,     0,        False),
 
-("m",  "contains/cg_text",       1, 0, BOOLEAN,     0,        False),
+("m",  "contains/cg_text",       1, 0, BOOLEAN,     0,        False),              # TODO: Create ENUMS for contains/* (full frontal etc.)
 ("m",  "contains/nudity",        1, 0, BOOLEAN,     0,        False),
+("m",  "contains/violence",      1, 0, BOOLEAN,     0,        False),
+
 #
 # "FMT" name space:
 # Asset technical metadata.
@@ -106,11 +117,11 @@ BASE_META_SET = [
 #
 
 ("fmt", "duration",              0, 0, TIMECODE,    0 ,       False),              # Clip duration. From ffprobe/format/duration. if fails, taken from streams[0]/duration
-("fmt", "file/mtime",            0, 0, DATETIME,    0 ,       False),
-("fmt", "file/size",             0, 0, INTEGER,     0 ,       False),
+("fmt", "file/mtime",            0, 0, DATETIME,    0 ,       False),              # Timestamp of file last modification
+("fmt", "file/size",             0, 0, INTEGER,     0 ,       False),              # File size in bytes
 ("fmt", "file/format",           0, 0, TEXT,        "",       False),              # Container format name. from ffprobe/format/format_name
-("fmt", "video/width",           0, 0, INTEGER,     0 ,       False),
-("fmt", "video/height",          0, 0, INTEGER,     0 ,       False),
+("fmt", "video/width",           0, 0, INTEGER,     0 ,       False),              # Video frame / image width (pixels)
+("fmt", "video/height",          0, 0, INTEGER,     0 ,       False),              # Video frame / image height (pixels)
 ("fmt", "video/fps",             0, 0, FRACTION,    "",       False),
 ("fmt", "video/pixel_format",    0, 0, TEXT,        "",       False),
 ("fmt", "video/aspect_ratio",    0, 0, FRACTION,    "",       False),
@@ -123,7 +134,7 @@ BASE_META_SET = [
 # Should be reset on media file change
 #
 
-("qc", "qc/state",               1, 0, ENUM,        0,        {0 : "New", 3 : "Rejected", 4 : "Approved (Inactive)", 5 : "Approved (Active)"}),   # 1 and 2 are reserved for Auto QC failed and passed states
+("qc", "qc/state",               1, 0, ENUM,        0,        {0 : "New", 3 : "Rejected", 4 : "Approved"}),   # 1 and 2 are reserved for Auto QC failed and passed states
 ("qc", "qc/report",              1, 0, BLOB,        "",       False),              # Holds error report from QC Pass and/or rejection/approval message from QC humanoid
 ("qc", "audio/bpm",              0, 0, NUMERIC,     0,        False),              # Music BPM
 ("qc", "audio/r128/i",           0, 0, NUMERIC,     0,        False),              # Integrated loudness (LUFS)
@@ -143,6 +154,9 @@ BASE_META_SET = [
 
 
 META_ALIASES = [
+
+### Tohle se musi syncnout s tema tagama nahore (le bordel)
+
 ("id_object"            , "en-US", "Object ID",         "#"),
 ("media_type"           , "en-US", "Media type",        None),
 ("content_type"         , "en-US", "Content type",      ""),
@@ -152,41 +166,78 @@ META_ALIASES = [
 ("origin"               , "en-US", "Origin",            None),
 ("version_of"           , "en-US", "Version of",        None),
 ("status"               , "en-US", "Status",            None),
+
 ("id_storage"           , "en-US", "Storage",           None),
 ("path"                 , "en-US", "Path",              None),
 ("state"                , "en-US", "Approval",          None),
-("script/rundown"       , "en-US", "Script",            None),
 ("mark_in"              , "en-US", "Mark in",           None),
 ("mark_out"             , "en-US", "Mark out",          None),
 ("subclips"             , "en-US", "Subclips",          None),
 ("article"              , "en-US", "Text",              None),
+
 ("title"                , "en-US", "Title",             None),
 ("title/subtitle"       , "en-US", "Subtitle",          None),
 ("title/alternative"    , "en-US", "Alt. title",        None),
 ("title/series"         , "en-US", "Series",            None),
 ("title/original"       , "en-US", "Orig. title",       None),
+("description"          , "en-US", "Description",       None),
+("promoted"             , "en-US", "promoted",          ""),
+
+
+("language"             , "en-US", "Language",          None),
+("date"                 , "en-US", "Date",              None),
+("subject"              , "en-US", "Keywords",          None),
+("rights"               , "en-US", "Rights",            None),
+("version"              , "en-US", "Version",           None),
+
+("format"               , "en-US", "Format",            None),
+("genre"                , "en-US", "Genre",             None),
+
 ("identifier/main"      , "en-US", "IDEC",              None),
 ("identifier/youtube"   , "en-US", "Youtube ID",        None),
 ("identifier/vimeo"     , "en-US", "Vimeo ID",          None),
-("language"             , "en-US", "Language",          None),
-("date"                 , "en-US", "Date",              None),
-("genre"                , "en-US", "Genre",             None),
+
+("role/director"        , "en-US", "Director",          None),
 ("role/performer"       , "en-US", "Artist",            None),
-("description"          , "en-US", "Description",       None),
-("coverage"             , "en-US", "Coverage",          None),
-("rights"               , "en-US", "Rights",            None),
-("version"              , "en-US", "Version",           None),
+("role/composer"        , "en-US", "Composer",          None),
+
 ("album"                , "en-US", "Album",             None),
-("file/mtime"           , "en-US", "File changed",      None),
-("file/size"            , "en-US", "File size",         None),
-("format"               , "en-US", "Format",            None),
-("duration"             , "en-US", "Duration",          None),
-("promoted"             , "en-US", "promoted",          ""),
-("rundown_symbol"       , "en-US", "Rundown symbol",    ""),
-("rundown_status"       , "en-US", "Status",            None),
-("rundown_broadcast"    , "en-US", "Broadcast time",    "Broadcast"),
-("rundown_scheduled"    , "en-US", "Scheduled time",    "Scheduled"),
-("run_mode"             , "en-US", "Run mode",          "Run"),
-("qc/state"             , "en-US", "State",             "QC"),
+## TODO : Zbytek od alba , contains....
+
+
+### Odsud dal by to melo sedet
+
+("rundown_symbol"       , "en-US", "Rundown symbol",                         ""),
+("rundown_status"       , "en-US", "Status",                                 None),
+("rundown_broadcast"    , "en-US", "Broadcast time",                         "Broadcast"),
+("rundown_scheduled"    , "en-US", "Scheduled time",                         "Scheduled"),
+("run_mode"             , "en-US", "Run mode",                               "Run"),
+
+("file/format"          , "en-US", "File format",                            None),
+("file/mtime"           , "en-US", "File changed",                           None),
+("file/size"            , "en-US", "File size",                              None),
+("duration"             , "en-US", "Duration",                               None),
+("video/width"          , "en-US", "Width",                                  None),
+("video/height"         , "en-US", "Height",                                 None),
+("video/fps"            , "en-US", "Frame rate",                             "FPS"),
+("video/pixel_format"   , "en-US", "Pixel format",                           None),
+("video/aspect_ratio"   , "en-US", "Aspect ratio",                           "Aspect"),
+("video/codec"          , "en-US", "Video codec",                            None),
+("audio/codec"          , "en-US", "Audio codec",                            None),
+
+("qc/state"             , "en-US", "QC State",                               "QC"),
+("qc/report"            , "en-US", "QC Report",                              None),
+("audio/bpm"            , "en-US", "BPM",                                    None),
+("audio/r128/i"         , "en-US", "Integrated loudness (LUFS)",             "Loudness"),
+("audio/r128/t"         , "en-US", "Integrated loudness threshold (LUFS)",   "ILT"),
+("audio/r128/lra"       , "en-US", "LRA (LU)",                               "LRA"),
+("audio/r128/lra/t"     , "en-US", "Loudness range threshold (LUFS)",        "LRT"),
+("audio/r128/lra/l"     , "en-US", "LRA Low (LUFS)",                         "LRA-L"),
+("audio/r128/lra/r"     , "en-US", "LRA High (LUFS)",                        "LRA-H"),
+("audio/silence"        , "en-US", "Areas with silent audio",                "Silences"),
+("audio/clipping"       , "en-US", "Audio clipping areas",                   "Clipping"),
+("video/black"          , "en-US", "Areas where video is black-only",        "Blacks"),
+("video/static"         , "en-US", "Areas with static image",                "Statics"),
+
 ]
 
